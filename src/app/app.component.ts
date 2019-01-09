9fc0cad1fcc89f07a15782660a4118a9bb199454
elise <elise.marie.lau@gmail.com>
2019-01-09 16:35:47 +0100

    google auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { Router } from '@angular/router';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
@@ -12,15 +14,28 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private nativeStorage: NativeStorage,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      //Here we will check if the user is already logged in
+      //because we don't want to ask users to log in each time they open the app
+      this.nativeStorage.getItem('google_user')
+      .then( data => {
+        //user is previously logged and we have his data
+        //we will let him access the app
+        this.router.navigate(["/home"]);
+        this.splashScreen.hide();
+      }, err => {
+        this.router.navigate(["/login"]);
+        this.splashScreen.hide();
+      })
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
     });
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: LoginPage
   }
 ];
 
@@ -18,13 +19,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
+    RouterModule.forChild(routes)
   ],
-  declarations: [HomePage]
+  declarations: [LoginPage]
 })
-export class HomePageModule {}
+export class LoginPageModule {}
