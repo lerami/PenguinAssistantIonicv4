88ae129a6ebe654bf357aee8fa3cb38591926589
elise <elise.marie.lau@gmail.com>
2019-01-09 16:43:15 +0100

    push on remote

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,7 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  user: any;
+  user: any; 
   userReady: boolean = false;
 
   constructor(
