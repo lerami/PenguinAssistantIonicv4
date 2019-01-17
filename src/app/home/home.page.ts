28c6b60d96714d525878e347dfc160330f7e3426
elise <elise.marie.lau@gmail.com>
2019-01-17 10:14:38 +0100

    reorganized home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,8 +11,8 @@ import { HTTP } from '@ionic-native/http';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  connections: any; 
-  connectionsReady: boolean = false; 
+  connections: any;
+  connectionsReady: boolean = false;
 
   constructor(
     private googlePlus: GooglePlus,
@@ -26,33 +26,33 @@ export class HomePage implements OnInit {
     const loading = await this.loadingController.create({
       message: 'Please wait...'
     });
-     await loading.present();
-     this.nativeStorage.getItem('access_token')
-    .then(access_token => {
-      this.http.get('https://people.googleapis.com/v1/people/me/connections', {personFields: 'emailAddresses'}, {Accept: 'application/json', Authorization: access_token})
-      .then(res => {
-        this.connections = res.data.connections;
-        this.connectionsReady = true;
-      })
-      .catch(err => {
-        console.log(err.status, err.message, err.headers);
-      })
-      loading.dismiss();
-    }, error =>{
-      console.log(error);
-      loading.dismiss();
-    });
+    await loading.present();
+    this.nativeStorage.getItem('access_token')
+      .then(access_token => {
+        this.http.get('https://people.googleapis.com/v1/people/me/connections', { personFields: 'emailAddresses' }, { Accept: 'application/json', Authorization: access_token })
+          .then(res => {
+            this.connections = res.data.connections;
+            this.connectionsReady = true;
+          })
+          .catch(err => {
+            console.log(err.status, err.message, err.headers);
+          })
+        loading.dismiss();
+      }, error => {
+        console.log(error);
+        loading.dismiss();
+      });
   }
 
-  doGoogleLogout(){
+  doGoogleLogout() {
     this.googlePlus.logout()
-    .then(res => {
-      //user logged out so we will remove him from the NativeStorage
-      this.nativeStorage.remove('access_token');
-      this.nativeStorage.remove('google_user');
-      this.router.navigate(["/login"]);
-    }, err => {
-      console.log(err);
-    });
+      .then(res => {
+        //user logged out so we will remove him from the NativeStorage
+        this.nativeStorage.remove('access_token');
+        this.nativeStorage.remove('google_user');
+        this.router.navigate(["/login"]);
+      }, err => {
+        console.log(err);
+      });
   }
 }
